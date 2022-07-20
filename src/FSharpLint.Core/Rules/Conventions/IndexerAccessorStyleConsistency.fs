ff70ed5c08e755af489ed22e3882cf6d2e6e8635
Parham Saremi <parhaamsaremi@gmail.com>
2022-07-20 14:34:24 +0430

    Add new rule IndexerAccessorStyleConsistency for OCaml and CSharp styles
    Fixes https://github.com/fsprojects/FSharpLint/issues/532

---
+++ src/FSharpLint.Core/Rules/Conventions/IndexerAccessorStyleConsistency.fs
@@ -0,0 +1,52 @@
+module FSharpLint.Rules.IndexerAccessorStyleConsistency
+
+open FSharp.Compiler.Syntax
+open FSharpLint.Framework.Ast
+open FSharpLint.Framework.AstInfo
+open FSharpLint.Framework.Rules
+open FSharpLint.Framework.Suggestion
+open System
+
+[<RequireQualifiedAccess>]
+type Config = {
+    Style: string
+}
+
+let generateOutput (range: FSharp.Compiler.Text.Range) (styleMain: string) (styleError: string) =
+    { Range = range
+      Message = String.Format ("You must use %s styling instead of %s for indexer accessing", styleMain, styleError)
+      SuggestedFix = None
+      TypeChecks = List.Empty } |> Array.singleton
+
+let runner (config: Config) (args: AstNodeRuleParams) =
+    let styleType = config.Style
+    if String.Equals (styleType, "ocaml", StringComparison.InvariantCultureIgnoreCase) then
+        match args.AstNode with
+        | AstNode.Binding binding ->
+            match binding with
+            | SynBinding (_, _, _, _, _, _, _, SynPat.Named _, _
+                , SynExpr.App (ExprAtomicFlag.Atomic, _, SynExpr.Ident _, SynExpr.ArrayOrListOfSeqExpr (_, expr, range), _), _, _) ->
+                generateOutput range "OCaml" "CSharp"
+            | _ ->
+                Array.empty
+        | _ -> 
+            Array.empty
+    elif String.Equals (styleType, "csharp", StringComparison.InvariantCultureIgnoreCase) then
+        match args.AstNode with
+        | AstNode.Binding binding ->
+            match binding with
+            | SynBinding (_, _, _, _, _, _, _, SynPat.Named _, _
+                , SynExpr.DotIndexedGet (_, _, _, range), _, _) ->
+                generateOutput range "CSharp" "OCaml"
+            | _ ->
+                Array.empty
+        | _ -> 
+            Array.empty
+    else
+        failwith (String.Format("Unknown style type %s", styleType))
+
+let rule config =
+    { Name = "IndexerAccessorStyleConsistency"
+      Identifier = Identifiers.IndexerAccessorStyleConsistency
+      RuleConfig = { AstNodeRuleConfig.Runner = runner config; Cleanup = ignore } }
+    |> AstNodeRule
